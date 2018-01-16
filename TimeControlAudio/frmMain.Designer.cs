4be1c635f48c815f90d3408edb18330676d21ae3
Thiago Martins de Sousa <thiago@p21sistemas.com.br>
2018-01-16 14:40:35 -0200

    Commit Inicial

---
+++ TimeControlAudio/frmMain.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace TimeControlAudio
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.htmlToolTip1 = new MetroFramework.Drawing.Html.HtmlToolTip();
+            this.btIniciar = new MetroFramework.Controls.MetroTile();
+            this.btParar = new MetroFramework.Controls.MetroTile();
+            this.txtTempo = new System.Windows.Forms.MaskedTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // htmlToolTip1
+            // 
+            this.htmlToolTip1.OwnerDraw = true;
+            // 
+            // btIniciar
+            // 
+            this.btIniciar.ActiveControl = null;
+            this.btIniciar.Location = new System.Drawing.Point(137, 178);
+            this.btIniciar.Name = "btIniciar";
+            this.btIniciar.Size = new System.Drawing.Size(98, 60);
+            this.btIniciar.Style = MetroFramework.MetroColorStyle.Green;
+            this.btIniciar.TabIndex = 0;
+            this.btIniciar.Text = "Iniciar";
+            this.btIniciar.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
+            this.btIniciar.UseSelectable = true;
+            this.btIniciar.Click += new System.EventHandler(this.btIniciar_Click);
+            // 
+            // btParar
+            // 
+            this.btParar.ActiveControl = null;
+            this.btParar.Location = new System.Drawing.Point(241, 178);
+            this.btParar.Name = "btParar";
+            this.btParar.Size = new System.Drawing.Size(98, 60);
+            this.btParar.Style = MetroFramework.MetroColorStyle.Red;
+            this.btParar.TabIndex = 1;
+            this.btParar.Text = "Parar";
+            this.btParar.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
+            this.btParar.UseSelectable = true;
+            this.btParar.Click += new System.EventHandler(this.btParar_Click);
+            // 
+            // txtTempo
+            // 
+            this.txtTempo.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTempo.Location = new System.Drawing.Point(23, 74);
+            this.txtTempo.Mask = "90:00:00";
+            this.txtTempo.Name = "txtTempo";
+            this.txtTempo.Size = new System.Drawing.Size(316, 98);
+            this.txtTempo.TabIndex = 2;
+            this.txtTempo.Text = "000000";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 261);
+            this.Controls.Add(this.txtTempo);
+            this.Controls.Add(this.btParar);
+            this.Controls.Add(this.btIniciar);
+            this.Name = "frmMain";
+            this.Text = "Controlador tempo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Drawing.Html.HtmlToolTip htmlToolTip1;
+        private MetroFramework.Controls.MetroTile btIniciar;
+        private MetroFramework.Controls.MetroTile btParar;
+        private System.Windows.Forms.MaskedTextBox txtTempo;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ TimeControlAudio/frmMain.cs
@@ -0,0 +1,118 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeControlAudio
+{
+    public partial class frmMain : MetroForm
+    {
+        public int soma;
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btIniciar_Click(object sender, EventArgs e)
+        {
+            muteOff();
+            timer1.Interval = 1000;
+
+            string[] tempo = txtTempo.Text.Split(':');
+
+            int horas = Convert.ToInt32(tempo[0]);
+            int minutos = Convert.ToInt32(tempo[1]);
+            int segundos = Convert.ToInt32(tempo[2]);
+
+            soma = (horas * 3600) + (minutos * 60) + segundos;
+            timer1.Start();
+        }
+
+        private void btParar_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            soma -= 1;
+            string[] values = txtTempo.Text.Split(':');
+
+            TimeSpan ts = TimeSpan.FromSeconds(soma);
+
+            txtTempo.Text = ts.Hours.ToString().PadLeft(2, '0') +
+                ts.Minutes.ToString().PadLeft(2, '0') +
+                ts.Seconds.ToString().PadLeft(2, '0');
+
+            if (soma == 0)
+            {
+                timer1.Stop();
+                muteOn();
+            }
+        }
+        private void muteOn()
+        {
+            try
+            {
+                //Instantiate an Enumerator to find audio devices
+                NAudio.CoreAudioApi.MMDeviceEnumerator MMDE = new NAudio.CoreAudioApi.MMDeviceEnumerator();
+                //Get all the devices, no matter what condition or status
+                NAudio.CoreAudioApi.MMDeviceCollection DevCol = MMDE.EnumerateAudioEndPoints(NAudio.CoreAudioApi.DataFlow.All, NAudio.CoreAudioApi.DeviceState.All);
+                //Loop through all devices
+                foreach (NAudio.CoreAudioApi.MMDevice dev in DevCol)
+                {
+                    try
+                    {
+                        //Show us the human understandable name of the device
+                        System.Diagnostics.Debug.Print(dev.FriendlyName);
+                        //Mute it
+                        dev.AudioEndpointVolume.Mute = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        //Do something with exception when an audio endpoint could not be muted
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //When something happend that prevent us to iterate through the devices
+            }
+        }
+
+        private void muteOff()
+        {
+            try
+            {
+                //Instantiate an Enumerator to find audio devices
+                NAudio.CoreAudioApi.MMDeviceEnumerator MMDE = new NAudio.CoreAudioApi.MMDeviceEnumerator();
+                //Get all the devices, no matter what condition or status
+                NAudio.CoreAudioApi.MMDeviceCollection DevCol = MMDE.EnumerateAudioEndPoints(NAudio.CoreAudioApi.DataFlow.All, NAudio.CoreAudioApi.DeviceState.All);
+                //Loop through all devices
+                foreach (NAudio.CoreAudioApi.MMDevice dev in DevCol)
+                {
+                    try
+                    {
+                        //Show us the human understandable name of the device
+                        System.Diagnostics.Debug.Print(dev.FriendlyName);
+                        //Mute it
+                        dev.AudioEndpointVolume.Mute = false;
+                    }
+                    catch (Exception ex)
+                    {
+                        //Do something with exception when an audio endpoint could not be muted
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //When something happend that prevent us to iterate through the devices
+            }
+        }
+    }
+}
